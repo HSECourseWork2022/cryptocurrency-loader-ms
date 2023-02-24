a23a68d3b3ec22e1156367062dd6f2d35a735f7f
yawkar <yawkarpov@gmail.com>
2023-02-24 17:40:47 +0300

    add persistence annotations

--- src/main/java/dev/hse/loader/pojo/entity/CryptocurrencyRecord.java
+++ src/main/java/dev/hse/loader/pojo/entity/CryptocurrencyRecord.java
@@ -6,27 +6,35 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@Entity(name = "cryptocurrency_record")
 @NoArgsConstructor
 @AllArgsConstructor
 public class CryptocurrencyRecord {
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC+3")
+    @Column(name = "observed_at")
     LocalDateTime observedAt;
+    @Column(name = "symbol")
     String symbol;
+    @Column(name = "vendor")
     Long vendor;
+    @Column(name = "max_supply")
     Long maxSupply;
+    @Column(name = "circulating_supply")
     Long circulatingSupply;
+    @Column(name = "total_supply")
     Long totalSupply;
+    @Column(name = "price_usd")
     Double priceUSD;
+    @Column(name = "volume_24h")
     Double volume24h;
+    @Column(name = "market_cap")
     Double marketCap;
 }
