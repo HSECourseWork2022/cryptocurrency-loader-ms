2467f6f42d6beefe6e3e8bc5b57159ee45bbfe7b
Dmitry Kutuzov <archie-woc@ya.ru>
2023-04-25 21:07:56 +0300

    add metrics for cryptocurrencies

--- src/main/java/dev/hse/loader/service/LoaderService.java
+++ src/main/java/dev/hse/loader/service/LoaderService.java
@@ -2,10 +2,16 @@ package dev.hse.loader.service;
 
 import dev.hse.loader.mapper.CryptocurrencyMapper;
 import dev.hse.loader.pojo.CryptocurrencyFiltered;
+import dev.hse.loader.pojo.entity.CryptocurrencyRecord;
 import dev.hse.loader.repository.CryptocurrencyRepository;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j(topic = "[loader][cryptocurrency][processing]")
 @Service
@@ -14,10 +20,26 @@ public class LoaderService {
 
     private final CryptocurrencyRepository repository;
     private final CryptocurrencyMapper mapper;
+    private final MeterRegistry registry;
+    private final Map<String, CryptocurrencyCollectorService> getCollectorFromSymbol = new HashMap<>();
 
     public void process(CryptocurrencyFiltered cryptocurrencyFiltered) {
         log.info("Processing: {}", cryptocurrencyFiltered);
-        var savedEntity = repository.save(mapper.toRecord(cryptocurrencyFiltered));
+        CryptocurrencyRecord record = mapper.toRecord(cryptocurrencyFiltered);
+        var savedEntity = repository.save(record);
+
+        CryptocurrencyCollectorService collectorService = getCollectorFromRecord(record);
+        collectorService.updatePrice(record.getPriceUSD().intValue());
         log.info("Saved new record: {}", savedEntity);
+    }
+
+    private CryptocurrencyCollectorService getCollectorFromRecord(CryptocurrencyRecord record) {
+        CryptocurrencyCollectorService collectorService;
+        String symbol = record.getSymbol();
+        if (!getCollectorFromSymbol.containsKey(symbol)) {
+            collectorService = new CryptocurrencyCollectorService(symbol, registry);
+            getCollectorFromSymbol.put(symbol, collectorService);
+        }
+        return getCollectorFromSymbol.get(symbol);
     }
 }
